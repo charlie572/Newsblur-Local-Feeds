89afdf2357e929364e53e2746c999c6899cbd142
Charlie <psycb8@nottingham.ac.uk>
2025-09-01 10:53:12 +0100

    Remove trainer storage methods

--- src/storage.js
+++ src/storage.js
@@ -142,94 +142,6 @@ export async function delete_feed_in_folder(feed_id, folder) {
     await browser.storage.local.set(result);
 }
 
-export async function get_feeds_trainer(feed_id) {
-    const result = await browser.storage.local.get(
-        ["local_feeds_trainers", "local_stories"]
-    );
-
-    if (feed_id in result.local_feeds_trainers) {
-        return result.local_feeds_trainers[feed_id];
-    }
-
-    const stories = result.local_stories[feed_id];
-
-    const milliseconds_per_month = 1000 * 60 * 60 * 24 * 30;
-    const stories_last_month = stories.filter(s => Date.now() - s.story_timestamp * 1000 < milliseconds_per_month);
-
-    const trainer = {
-        classifiers: {
-            feeds: {},
-            authors: {},
-            titles: {},
-            tags: {},
-        },
-        feed_id: feed_id,
-        stories_last_month: stories_last_month.length,
-        num_subscribers: 1,
-        feed_tags: [],
-        feed_authors: [],
-    };
-
-    result.local_feeds_trainers[feed_id] = trainer;
-
-    await browser.storage.local.set(
-        {local_feeds_trainers: result.local_feeds_trainers}
-    );
-
-    return trainer;
-}
-
-export async function save_classifier(classifier_update) {
-    const result = await browser.storage.local.get("local_feeds_trainers");
-
-    const feed_id = classifier_update.feed_id;
-    const trainer = result.local_feeds_trainers[feed_id];
-
-    for (const update_key in classifier_update) {
-        if (update_key === "feed_id") continue;
-
-        // list of entities to update
-        const update_ents = classifier_update[update_key];
-
-        const update_key_split = update_key.split("_");
-
-        // check whether we are adding or removign rules
-        const remove = update_key_split[0] === "remove";
-
-        // unpack update key
-        // update_like is either "like" or "dislike". It says which one 
-        // the update targets.
-        // update_type is "author", "feed", "tag", or "title"
-        if (remove) update_key_split.shift();
-        const [update_like, update_type] = update_key_split;
-
-        // In the trainer, likes or dislikes are represented by 1 or -1.
-        const update_like_num = (update_like === "like") ? 1 : -1;
-
-        // get the trainer object for this update_type
-        // trainer_ents is an object of entities, with 
-        // the entity as its key, and 1 or -1 for its value, 
-        // to represent like or dislike.
-        const trainer_classifier_key = update_type + "s";
-        const trainer_ents = trainer.classifiers[trainer_classifier_key];
-
-        // Either add or remove rules.
-        if (remove) {
-            for (const trainer_ent in trainer_ents) {
-                if (update_ents.includes(trainer_ent)) {
-                    delete trainer_ents[trainer_ent];
-                }
-            }
-        } else {
-            for (const update_ent of update_ents) {
-                trainer_ents[update_ent] = update_like_num;
-            }
-        }
-    }
-
-    await browser.storage.local.set(result);
-}
-
 export async function get_local_feeds() {
     const result = await browser.storage.local.get("local_feeds");
     return result.local_feeds;
@@ -245,14 +157,12 @@ export async function setup_storage() {
     const result = await browser.storage.local.get([
         "local_feeds", 
         "local_stories", 
-        "local_feeds_trainers",
         "next_feed_id",
     ]);
 
     browser.storage.local.set({
         local_feeds: result.local_feeds || {},
         local_stories: result.local_stories || {},
-        local_feeds_trainers: result.local_feeds_trainers || {},
         next_feed_id: result.next_feed_id || -1,
     });
 }
